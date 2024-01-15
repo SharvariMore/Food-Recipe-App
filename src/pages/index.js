2176ea91182f5bef02c6446f48f60f2d6e32768a
Sharvari More <83008601+SharvariMore@users.noreply.github.com>
2024-01-15 16:01:31 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import HomePage from "../pages/HomePage/HomePage";
-import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import MealDetailsPage from "../pages/MealDetailsPage/MealDetailsPage";
 import CategoryPage from "../pages/CategoryPage/CategoryPage";
 
-export { HomePage, ErrorPage, MealDetailsPage, CategoryPage };
+export { HomePage, MealDetailsPage, CategoryPage };
