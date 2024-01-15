f852426ed9657075d436f4d492066fb9859307cd
Sharvari More <83008601+SharvariMore@users.noreply.github.com>
2024-01-15 15:35:56 -0500

    Imported all the files in one file

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import HomePage from "../pages/HomePage/HomePage";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import MealDetailsPage from "../pages/MealDetailsPage/MealDetailsPage";
+import CategoryPage from "../pages/CategoryPage/CategoryPage";
+
+export { HomePage, ErrorPage, MealDetailsPage, CategoryPage };
